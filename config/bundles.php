a0c8e2ae9453b59720d2f67ad8645e144f23f98e
aurelien <aurelien.dubuisson@sciencespo.fr>
2026-03-03 15:55:57 +0100

    feat: require security

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
